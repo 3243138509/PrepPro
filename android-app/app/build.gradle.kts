b36f1eccfc90cf7a7247055ba910838ee7bf00c4
3243138509 <140893582+3243138509@users.noreply.github.com>
2026-03-15 22:29:14 +0800

    1

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 3
-        versionName = "1.0.3"
+        versionName = "1.0.4"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
         }
